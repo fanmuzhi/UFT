6eb4ee44a418e066d5893dee607c32e62ed12a36
countrymanbb <boqiling@gmail.com>
2014-11-06 16:26:57 +0800

    rewrite i2c_basic to PGEMBasic class

--- src/models/base.py
+++ src/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
+import logging
 
+logger = logging.getLogger(__name__)
 
 EEP_MAP = [{"name": "TEMPHIST", "addr": 0x000, "length": 2, "type": "int"},
            {"name": "CAPHIST", "addr": 0x021, "length": 32, "type": "int"},
@@ -20,6 +22,10 @@ EEP_MAP = [{"name": "TEMPHIST", "addr": 0x000, "length": 2, "type": "int"},
            {"name": "INITIALCAP", "addr": 0x077, "length": 1, "type": "int"}]
 
 
+class PGEMException(Exception):
+    pass
+
+
 class PGEMBase(object):
 
     def __init__(self, device,  **kvargs):
@@ -35,10 +41,6 @@ class PGEMBase(object):
         # I2C adapter device
         self.device = device
 
-        # Switch I2C connection to current PGEM
-        # Need call this function every time before communicate with PGEM
-        self.switch()
-
     def switch(self):
         """switch I2C ports by PCA9548A, only 1 channel is enabled.
         chnum(channel number): 0~7
@@ -48,9 +50,25 @@ class PGEMBase(object):
         slot = self.slot
         self.device.slave_addr = 0x70 + chnum    # 0111 0000
         wdata = [0x01 << slot]
+
+        # Switch I2C connection to current PGEM
+        # Need call this function every time before communicate with PGEM
         self.device.write(wdata)
 
-    def _query_map(self, mymap, **kvargs):
+    def switch_back(self):
+        """switch I2C ports back to mother board
+           chnum(channel number): 0~7
+        """
+        chnum = self.channel
+        self.device.slave_addr = 0x70 + chnum    # 0111 0000
+        wdata = 0x00
+
+        # Switch I2C connection to mother board
+        # Need call this function every time before communicate with mother board
+        self.device.write(wdata)
+
+    @staticmethod
+    def _query_map(mymap, **kvargs):
         """method to search the map (the list of dict, [{}, {}])
         params: mymap:  the map to search
                 kvargs: query conditon key=value, key should be in the dict.
@@ -61,7 +79,7 @@ class PGEMBase(object):
             r = filter(lambda row: row[k] == v,  r)
         return r
 
-    def read_VPD_byname(self, reg_name):
+    def read_vpd_byname(self, reg_name):
         """method to read eep_data according to eep_name
         eep is one dict in eep_map, for example:
         {"name": "CINT", "addr": 0x02B3, "length": 1, "type": "int"}
@@ -79,7 +97,7 @@ class PGEMBase(object):
         if(typ == "int"):
             return datas[0]
 
-    def read_VPD(self):
+    def read_vpd(self):
         """method to read out EEPROM info from dut
         return a dict.
         """
@@ -87,48 +105,205 @@ class PGEMBase(object):
         dut = {}
         for eep in EEP_MAP:
             reg_name = eep["name"]
-            dut.update({reg_name: self.read_VPD_byname(reg_name)})
+            dut.update({reg_name: self.read_vpd_byname(reg_name)})
         return dut
 
-    def write_VPD(self):
+    def write_vpd(self):
         pass
 
-    def control_LED(self):
+    def control_led(self, status="off"):
+        """method to control the LED on DUT chip PCA9536DP
+           status=1, LED off, default.
+           staus=0, LED on.
+        """
+        LOGIC = {"on": 0, "off": 1}
+        status = LOGIC.get(status)
+        if(status is None):
+            raise PGEMException("wrong LED status is set")
+
+        self.device.slave_addr = 0x41
+        REG_OUTPUT = 0x01
+        REG_CONFIG = 0x03
+
+        # config PIO to output
+        wdata = [REG_CONFIG, 0x00]
+        self.device.write(wdata)
+
+        # set LED status
+        out = status << 1
+        wdata = [REG_OUTPUT, out]
+        self.device.write(wdata)
+
+    def self_discharge(self, IO=0):
+        """Controlled by I/O expander IC, address 0x41
+           when IO=0, not discharge;
+           when IO=1, discharge.
+        """
+        if(IO !=0 and IO !=1):
+            raise PGEMException("wrong self_discharge IO is set")
+
+        self.device.slave_addr = 0x41
+        REG_OUTPUT = 0x01
+        REG_CONFIG = 0x03
+
+        # config PIO to output
+        wdata = [REG_CONFIG, 0x00]
+        self.device.write(wdata)
+
+        # set IO status
+        wdata = [REG_OUTPUT, IO]
+        self.device.write(wdata)
+
+    def encrypted_ic(self):
         pass
 
-    def self_discharge(self):
-        # Controlled by I/O expander IC, address 0x41
-        # When IO=0, discharge;
-        # When IO=1, not discharge.
-        pass
+    def write_bq24707(self, reg_addr, wata):
+        """
+        write regsiter value to charge IC BQ24707
+        """
+        self.device.slave_addr = 0x09
 
-    def encrypted_IC(self):
-        pass
+        # write first low 8bits, then high 8bits
+        self.device.write_reg(reg_addr, [wata & 0x00FF, wata >> 8])
 
-    def charge(self):
-        pass
+    def read_bq24707(self, reg_addr):
+        """
+        read register value from charge IC BQ24707
+        """
+        self.device.slave_addr = 0x09
+        ata_in = self.device.read_reg(reg_addr, length=2)
+
+        # first low 8bits then high 8bits
+        val = (ata_in[1] << 8) + ata_in[0]
+        return val
+
+    def charge(self, start=True):
+        """
+        Send charge option to charge IC to start the charge.
+        Charge IC BQ24707 is used as default.
+        Override this function is use other IC instead.
+        """
+        # BQ24707 register address
+        CHG_OPT_ADDR = 0x12
+        CHG_CUR_ADDR = 0x14
+        CHG_VOL_ADDR = 0x15
+        INPUT_CUR_ADDR = 0x3F
+        MAN_ID_ADDR = 0xFE
+        DEV_ID_ADDR = 0xFF
+
+        # check IC
+        logger.debug(self.read_bq24707(MAN_ID_ADDR))
+        logger.debug(self.read_bq24707(DEV_ID_ADDR))
+
+        # write options
+        charge_option = 0x1990
+        if start:
+            charge_option &= ~(0x01)    # clear last bit
+        else:
+            charge_option |= 0x01   # set last bit
+        self.write_bq24707(CHG_OPT_ADDR, charge_option)
+        self.write_bq24707(CHG_CUR_ADDR, 0x01C0)
+        self.write_bq24707(CHG_VOL_ADDR, 0x1200)
+        self.write_bq24707(INPUT_CUR_ADDR, 0x0400)
+
+        # read back to check if written successfully
+        assert self.read_bq24707(CHG_OPT_ADDR) == charge_option
+        assert self.read_bq24707(CHG_CUR_ADDR) == 0x01C0
+        assert self.read_bq24707(CHG_VOL_ADDR) == 0x1200
+        assert self.read_bq24707(INPUT_CUR_ADDR) == 0x0400
 
     def load_discharge(self):
         pass
 
-    def auto_discharge(self):
-        # Controlled by Present I/O
-        # When IO=0, discharge;
-        # When IO=1, not discharge.
-        pass
+    def auto_discharge(self, IO=0):
+        """output PRESENT/AUTO_DISCH signal on TCA9555 on mother board.
+           When IO=0, discharge;
+           When IO=1, not discharge.
+        """
+        chnum = self.channel
+
+        self.device.slave_addr = 0x20 + chnum
+        REG_INPUT = 0x00
+        REG_OUTPUT = 0x02
+        REG_CONFIG = 0x06
+
+        # config PIO-0 to output and PIO-1 to input
+        # first PIO-0 then PIO-1
+        wdata = [REG_CONFIG, 0x00, 0xFF]
+        self.device.write(wdata)
+
+        # read current status
+        val = self.device.read_reg(REG_INPUT, length=2)
+        val = val[0]    # only need port 0 value
+
+        # set current slot
+        if(IO==1):
+            # set bit
+            val |= (IO << self.slot)
+        else:
+            # clear bit
+            val &= ~(0X01 << self.slot)
+
+        # output
+        # first PIO-0, then PIO-1
+        wdata = [REG_OUTPUT, val, 0xFF]
+        self.device.write(wdata)
+
+        # read status back
+        val = self.device.read_reg(REG_INPUT, length=2)
+        val = val[0]    # only need port 0 value
+        val = (val & (0x01 << self.slot)) >> self.slot
+        assert val == 0
 
     def check_power_fail(self):
-        pass
+        """check power_fail_int signal on TCA9555 on mother board
+        return true is power failed.
+        """
+        chnum = self.channel
+
+        self.device.slave_addr = 0x20 + chnum
+        REG_INPUT = 0x00
+        REG_OUTPUT = 0x02
+        REG_CONFIG = 0x06
+
+        # config PIO-0 to output and PIO-1 to input
+        # first PIO-0 then PIO-1
+        wdata = [REG_CONFIG, 0x00, 0xFF]
+        self.device.write(wdata)
+
+        # read reg_input
+        val = self.device.read_reg(REG_INPUT, length=2)
+        val = val[1]    # only need port 1 value
+
+        # check current slot
+        val = (val & (0x01 << self.slot)) >> self.slot
+        if(val == 0):
+            # PWR_OK
+            return False
+        else:
+            # PWR_FAIL
+            return True
 
     def check_temp(self):
         pass
 
 if __name__ == "__main__":
+    logging.basicConfig()
 
     from pyaardvark import Adapter
     adk = Adapter()
     adk.open(portnum=0)
-    DUT = PGEMBase(device=adk)
-    print DUT.read_VPD()
-    adk.close()
 
+    DUT = PGEMBase(device=adk, slot=1)
+    DUT.switch()
+    #print DUT.read_vpd()
+    DUT.control_led(status="off")
+
+    DUT.charge(start=False)
+    #DUT.self_discharge(IO=0)
+
+    DUT.switch_back()
+    print DUT.check_power_fail()
+    DUT.auto_discharge(IO=1)
+
+    adk.close()
