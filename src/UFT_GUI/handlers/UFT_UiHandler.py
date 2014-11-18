c2ed577d6f3491d6abb3aab18edcf880e949c456
fanmuzhi <fanmuzhi@gmail.com>
2014-11-18 14:39:46 +0800

    function handlers import to main

--- src/UFT_GUI/handlers/UFT_UiHandler.py
+++ src/UFT_GUI/handlers/UFT_UiHandler.py
@@ -12,16 +12,30 @@ from PyQt4.QtGui import QKeySequence, QIcon, QPixmap
 from PyQt4.QtCore import Qt
 from UFT_GUI.UFT_Ui import Ui_Form as UFT_UiForm
 
+import log_handler
+import mpl_handler
+import sql_handler
+
 class UFT_UiHandler(UFT_UiForm):
     def __init__(self, parent=None):
         UFT_UiForm.__init__(self)
-    
+        log_handler.XStream.stdout().messageWritten.connect(self.__append_formatData)
+        
+    def setupWidget(self, wobj):
+        wobj.setWindowIcon(QIcon(QPixmap("./res/icons/logo.png")))
+        
+        
+    def __append_formatData(self, data):
+        self.info_textBrowser.insertPlainText(time.strftime("%Y-%m-%d %X\t")+data)
+        self.info_textBrowser.moveCursor(QtGui.QTextCursor.End)
+        
     
     
 if __name__ == "__main__":
     a=QtGui.QApplication(sys.argv)
     Form = QtGui.QWidget()
-    w=UFT_UiForm()
+    w=UFT_UiHandler()
     w.setupUi(Form)
+    w.setupWidget(Form)
     Form.show()   
     sys.exit(a.exec_())  
--- src/UFT_GUI/main.py
+++ src/UFT_GUI/main.py
@@ -13,14 +13,21 @@ import sys
 from PyQt4.QtGui import QApplication
 from PyQt4 import QtGui, QtCore
 from UFT_GUI.handlers.UFT_UiHandler import UFT_UiHandler
-
+from UFT_GUI.handlers import log_handler
 
 class MainWidget(QtGui.QWidget):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self)
         self.ui = UFT_UiHandler()
         self.ui.setupUi(self)
-
+        self.ui.setupWidget(self)
+        self.__setupSignal()
+        
+    def __setupSignal(self):
+        '''for log display test, to be changed as "start" function later'''
+        self.ui.start_pushButton.clicked.connect(log_handler.test)
+        
+        
 def main():
     app = QApplication(sys.argv)
     app.setStyle("Plastique")
