81ee45e866988ff1c96db44a2a3aabeda371165d
boqiling <boqiling@gmail.com>
2014-11-25 21:51:09 +0800

    move api to pyaardvark.py __init__(), and test fsm

--- test/test_fsm.py
+++ test/test_fsm.py
@@ -7,16 +7,24 @@ __version__ = "0.1"
 __author__ = "@boqiling"
 
 from UFT.fsm import IFunc, States, StateMachine
+from UFT.devices import aardvark
+import logging
+
+TestStates = 0xA0
+
 
 class MainFunc(IFunc):
     def init(self):
+        self.device = aardvark.Adapter()
         print "init"
 
     def idle(self):
         print "idle"
 
-    def work(self):
-        print "work"
+    def work(self, states):
+        if(states == TestStates):
+            self.device.sleep(5)
+            print "work"
 
     def error(self):
         print "error"
@@ -25,10 +33,12 @@ class MainFunc(IFunc):
         print "exit"
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     m = MainFunc()
 
     f = StateMachine(m)
     f.en_queue(States.INIT)
     f.run()
 
+    f.en_queue(TestStates)
     f.en_queue(States.EXIT)
