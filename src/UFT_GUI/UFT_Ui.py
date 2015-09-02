00207d2d4954497f8e83dd39440b5f4e605a0344
fanmuzhi <fanmuzhi@gmail.com>
2015-09-02 10:43:05 +0800

    2015-07-10 - temperature sensor IC chip ID check canceled

--- src/UFT_GUI/UFT_Ui.py
+++ src/UFT_GUI/UFT_Ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '.\UFT_Ui.ui'
 #
-# Created: Fri Aug 14 10:52:48 2015
+# Created: Fri Aug 28 14:39:05 2015
 #      by: PyQt4 UI code generator 4.11.2
 #
 # WARNING! All changes made in this file will be lost!
